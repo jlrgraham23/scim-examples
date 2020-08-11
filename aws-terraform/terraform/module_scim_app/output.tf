07ca3f9ec68d955d8d9ce6a88892b6c06144e006
Justin L R Graham <jgraham@23andme.com>
2020-08-10 20:01:32 -0500

    Add outputs for IAM role identification information.

--- aws-terraform/terraform/module_scim_app/output.tf
+++ aws-terraform/terraform/module_scim_app/output.tf
@@ -1,3 +1,15 @@
 output "app_lb_dns_name" {
   value = aws_route53_record.app_lb.fqdn
 }
+
+output "iam_role_arn" {
+  value = aws_iam_role.app.arn
+}
+
+output "iam_role_id" {
+  value = aws_iam_role.app.id
+}
+
+output "iam_role_name" {
+  value = aws_iam_role.app.name
+}
